36e9ba1f863fa1b413be45bb241de8702c070b27
BraiNeu-F <faizfadlh28@gmail.com>
2023-06-25 14:14:19 +0700

    add sub menu transaction

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from stock import Stock
+import sys
 
 class Menu:
 	def main(self):
 		print("\n=========== Aplikasi Retail ===========")
 		print("1. Kelola Stok Barang")
 		print("2. Kelola Transaksi Konsumen")
+		print("\n0. Exit Program")
 
 		choice = input("\nPilih menu: ")
 
@@ -12,6 +14,8 @@ class Menu:
 			self.menu_stock()
 		elif choice == '2':
 			self.menu_transaksi()
+		elif choice == '0':
+			sys.exit()
 
 	def menu_stock(self):
 		while True:
@@ -33,8 +37,23 @@ class Menu:
 				self.main()
 
 	def menu_transaksi(self):
-		# code goes here
-		pass
+		while True:
+			print("\n=========== Kelola Transaksi Konsumen ===========")
+			print("1. Input Data Transaksi Baru")
+			print("2. Lihat Data Seluruh Transaksi Konsumen")
+			print("3. Lihat Data Transaksi Berdasarkan Subtotal")
+			print("\n0. Kembali")
+
+			choice = input("\nPilih menu: ")
+
+			if choice == '1':
+				Stock().create()
+			elif choice == '2':
+				Stock().restock()
+			elif choice == '3':
+				Stock().show()
+			elif choice == '0':
+				self.main()
 
 				
 # Jalankan aplikasi
